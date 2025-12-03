b39c2509d1df07a216522f6a833edc2ed163a04e
Enraa <enraaaery@gmail.com>
2025-12-02 21:39:13 -0800

    First Setup
    Hello world!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gagbot
+A bot for garbling messages sent in a certain channel on a certain server. 
---
+++ index.js
@@ -0,0 +1,20 @@
+const discord = require('discord.js')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const client = new discord.Client({
+    intents: [
+        discord.GatewayIntentBits.Guilds,
+        discord.GatewayIntentBits.GuildMessages,
+        discord.GatewayIntentBits.MessageContent,
+        discord.GatewayIntentBits.GuildMembers
+    ]
+})
+
+client.on("clientReady", async () => {
+    // This is run once we’re logged in!
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(process.env.DISCORDBOTTOKEN)
